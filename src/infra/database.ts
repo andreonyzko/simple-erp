7adeda9ded6eca27a7fe5b4b228d22139456e551
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2026-02-11 18:29:58 -0300

    refactor(infra): update database and main entry point to support new architecture

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -8,29 +8,30 @@ import type { Service } from "../domain/entities/service";
 import type { Transaction } from "../domain/entities/transaction";
 
 export class ERPDataBase extends Dexie {
-    calendar!: Table<CalendarEvent, number>;
-    clients!: Table<Client, number>;
-    suppliers!: Table<Supplier, number>;
-    products!: Table<Product, number>;
-    services!: Table<Service, number>;
-    purchases!: Table<Purchase, number>;
-    sales!: Table<Sale, number>;
-    transactions!: Table<Transaction, number>;
+  calendar!: Table<CalendarEvent, number>;
+  clients!: Table<Client, number>;
+  suppliers!: Table<Supplier, number>;
+  products!: Table<Product, number>;
+  services!: Table<Service, number>;
+  purchases!: Table<Purchase, number>;
+  sales!: Table<Sale, number>;
+  transactions!: Table<Transaction, number>;
 
-    constructor(){
-        super('erp-database');
+  constructor() {
+    super("erp-database");
 
-        this.version(1).stores({
-            calendar: "++id, title, date",
-            clients: "++id, name, document, phone, active",
-            suppliers: "++id, name, document, phone, active",
-            products: "++id, name, supplierId, stockControl, stock, cost, sellPrice, active",
-            services: "++id, name, price, active",
-            purchases: "++id, supplierId, date, totalValue, paymentStatus, status",
-            sales: "++id, clientId, date, totalValue, paymentStatus, status",
-            transactions: "++id, type, origin, date, referenceId, method, value"
-        })
-    }
+    this.version(1).stores({
+      calendar: "++id, title, date",
+      clients: "++id, name, document, phone, active",
+      suppliers: "++id, name, document, phone, active",
+      products:
+        "++id, name, supplierId, stockControl, stock, cost, sellPrice, active",
+      services: "++id, name, price, active",
+      purchases: "++id, supplierId, date, totalValue, status",
+      sales: "++id, clientId, date, totalValue, status",
+      transactions: "++id, title, type, origin, date, referenceId, value",
+    });
+  }
 }
 
 export const db = new ERPDataBase();
